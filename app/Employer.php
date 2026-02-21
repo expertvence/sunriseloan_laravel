115b15ec520faeeaa40909d4435bd9c26ae018a8
MuhammadSagor <sagorwdpf@gmail.com>
2026-02-21 14:56:30 +0600

    redeploy

--- app/Employer.php
+++ app/Employer.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employer extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'email', 'password', 'company_name', 'company_address', 'company_phone',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
